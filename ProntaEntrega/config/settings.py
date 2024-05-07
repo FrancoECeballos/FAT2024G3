fae0921e52303ba7252b6f8b2a9ad6f2338816ab
FrancoECeballos <franco.ceballos2005@gmail.com>
2024-05-07 14:16:47 -0300

    Correcciónes en el django docker

--- ProntaEntrega/config/settings.py
+++ ProntaEntrega/config/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'USER': 'itsv',
         'PASSWORD': 'itsvpassword',
         'HOST': 'pe-db',
-        'PORT': '3306',
+        'PORT': '3307',
     }
 }
 
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = '/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'ProntaEntregaReact/build/static')
 ]
