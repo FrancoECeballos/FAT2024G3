337f822f06ef085e389c2745bce63e7b2e2f0969
WeigandtTimoteo <weigandttimoteo@gmail.com>
2024-05-13 15:56:30 -0300

    Fuga el index

--- ProntaEntrega/config/settings.py
+++ ProntaEntrega/config/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILE_DIRS = [os.path.join(BASE_DIR, 'ProntaEntregaReact/build/static')]
+STATICFILE_DIRS = [os.path.join(BASE_DIR, 'ProntaEntregaReact/build')]
 STATIC_ROOT = '/'
 
 # Default primary key field type
