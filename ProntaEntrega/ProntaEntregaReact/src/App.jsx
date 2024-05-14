d7ce385020e3d18aa6153b7fb477ced0a3c50a75
WeigandtTimoteo <weigandttimoteo@gmail.com>
2024-05-14 08:53:03 -0300

    Lo necesario para que pesti labure

--- ProntaEntrega/ProntaEntregaReact/src/App.jsx
+++ ProntaEntrega/ProntaEntregaReact/src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
+
+//importacion de los componentes
+import Login from '../src/components/Login'
+
 import './App.css'
 
 function App() {
-
-
   return (
   <div>
 
